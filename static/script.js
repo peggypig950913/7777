d9502fb1b8b70e79d9036a1662897e9c34a3549a
peggypig950913 <pe0913gg@gmail.com>
2026-06-02 00:06:53 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,10 +1,16 @@
-const video = document.getElementById("video");
-
-async function startCamera() {
-
-    const stream = await navigator.mediaDevices.getUserMedia({
-        video:true
+function addAction(action) {
+    fetch("/update", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ action: action })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("jump").textContent = data.jump;
+        document.getElementById("squat").textContent = data.squat;
+        document.getElementById("knee").textContent = data.knee;
+        document.getElementById("plank").textContent = data.plank;
     });
-
-    video.srcObject = stream;
 }
