27ade572ea7ee785e7a14cd20c3f9c58ea6930d0
peggypig950913 <pe0913gg@gmail.com>
2026-06-02 00:15:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-# 全域計數器
-counter = {
-    "jump": 0,
-    "squat": 0,
-    "knee": 0,
-    "plank": 0
-}
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/update", methods=["POST"])
-def update():
-    action = request.json.get("action")
-    if action in counter:
-        counter[action] += 1
-    return jsonify(counter)
 
 if __name__ == "__main__":
     app.run(debug=True)
